b2d6e0a3857f1d22b1ee53cb2be386446a4a8bd6
Michael <realq86@gmail.com>
2017-08-27 22:03:55 -0700

    Breaking the model out into different file.

--- EdmodoInterview/EdmodoServer.swift
+++ EdmodoInterview/EdmodoServer.swift
@@ -81,42 +81,4 @@ fileprivate func createModel(responses:AnyObject) -> [Assignment]{
     return assignments
 }
 
-struct Assignment {
-    
-    var json: [String:AnyObject]!
-    
-    var id: Int! {
-        get {
-            return json["id"] as? Int ?? 0
-        }
-    }
-    var title: String! {
-        get {
-            return json["title"] as? String ?? "default_title"
-        }
-    }
-    var description: String! {
-        get {
-            return json["description"] as? String ?? "default_description"
-        }
-    }
-    var dueAt: Date!
-    {
-        get {
-            if let dateString = json["due_at"] as? String {
-                let date = dateFormatter.date(from: dateString)
-                print("date is \(date!)")
-                return date
-            }
-            return Date.distantFuture
-        }
-    }
-    
-    init(json:[String:AnyObject]) {
-        self.json = json
-    }
-    
-    init() {
-        json = [String:AnyObject]()
-    }
-}
+
